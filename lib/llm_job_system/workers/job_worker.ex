3b43363e5ff62770f24bb01fbc647a17ebf1d676
mwenje24 <mwenje24@example.com>
2026-07-20 15:37:27 +0300

    added a job worker and the job life cycle

---
+++ lib/llm_job_system/workers/job_worker.ex
@@ -0,0 +1,37 @@
+defmodule LlmJobSystem.Workers.JobWorker do
+  @moduledoc """
+  processes a single job
+  """
+
+  use GenServer
+
+  alias LlmJobSystem.Jobs.Job
+  alias LlmJobSystem.Jobs.JobQueue
+
+  def start_link(%Job{} = job) do
+    GenServer.start_link(__MODULE__, job)
+  end
+
+  @impl true
+  def init(job) do
+    send(self(), :process)
+
+    {:ok, job}
+  end
+
+  @impl true
+  def handle_info(:process, job) do
+    updated_job =
+      Job.complete(
+        job,
+        "The job has successfully completed"
+      )
+
+      JobQueue.update_job(updated_job)
+
+      send(LlmJobSystem.Jobs.Dispatcher, :dispatch)
+
+      {:stop, :normal, updated_job}
+  end
+
+end
